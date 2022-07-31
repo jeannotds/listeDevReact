ad05271907e1df26769c9aa1579ba1b3eafe4d0f
jeannotds <diambujeannot@gmail.com>
2022-07-31 12:51:57 +0100

    ajout du formulaire controle

--- src/composants/Formulaire.js
+++ src/composants/Formulaire.js
@@ -1,5 +1,7 @@
 import { useState } from "react"
 
+
+//FORMULAIRE NON CONTROLE
 function Form(e){
     e.preventDefault()
     alert(
@@ -7,17 +9,37 @@ function Form(e){
     )
 }
 
+
 function Formulaire(){
+    // const [inputValues, setInputValue] = useState('Posez votre question ici')
+    const [inputValue, setValue] = useState()
+
     return(
         <div>
+            {/* //FORMULAIRE NON CONTROLE */}
             <form onSubmit={Form}>
                 <input type="text" name="nom" placeholder="Entrer votre nom!"/>
                 <input type="text" name="prenom" placeholder="Entrer votre prenom!"/>
                 <button type="submit">Entrer</button>
             </form>
+
+
+            {/* //FORMULAIRE CONTROLE */}
+            <div>
+                <textarea value={inputValue}
+                    onChange={(e)=>{
+                        setValue(e.target.value)
+                    }}
+                />
+                <button
+                    onClick={()=>{
+                        alert(inputValue)
+                    }}
+                >Submit</button>
+            </div>
+
         </div>
     )
 }
-
 
 export default Formulaire
