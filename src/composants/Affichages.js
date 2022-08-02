be8b0ced349b3335c8f36436d5d64eaecc1235b9
jeannotds <diambujeannot@gmail.com>
2022-08-02 16:02:12 +0100

    les actions sur le composant Affichages

--- src/composants/Affichages.js
+++ src/composants/Affichages.js
@@ -1,14 +1,31 @@
+import { useState } from "react"
 
 
 
 function Affichages(){
 
+    const [value, setValue] = useState(0)
+    const [show, setShow] = useState(false)
+    const montant = 10
 
-    return (
+    return show ? (
         <div>
-            Affichages
+            <p onClick={()=>{
+                setShow(false)
+            }}>Cachez</p>
+
+            <h1>montant : {montant}</h1>
+            <button onClick={()=>{
+                setValue(value+1)
+            }}>Ajouter</button>
+
+            <p>La somme vaut : {value * montant}</p>
         </div>
-    )
+    ) : <div>
+            <p onClick={()=>{
+                setShow(true)
+            }}>Afficher</p>
+        </div>
 }
 
 export default Affichages
